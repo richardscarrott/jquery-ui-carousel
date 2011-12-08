400803602f963c26f5e494af84b855eed0e6852a
Richard Scarrott <riscarrott@googlemail.com>
2011-12-08 17:08:31 +0000

    #33 - don't run beforeAnimate and afterAnimate callbacks if animate is false

--- js/jquery.rs.carousel.js
+++ js/jquery.rs.carousel.js
@@ -427,8 +427,10 @@
         },
 
         _slide: function (animate) {
-        
-            this._trigger('beforeAnimate', null, this.elements);
+         
+            if (animate !== false) {
+                this._trigger('beforeAnimate', null, this.elements);
+            }
 
             var self = this,
                 speed = animate === false ? 0 : this.options.speed, // default to animate
@@ -448,8 +450,10 @@
             this.elements.runner
                 .stop()
                 .animate(animateProps, speed, this.options.easing, function () {
-
-                    self._trigger('afterAnimate', null, self.elements);
+                    
+                    if (animate !== false) {
+                        self._trigger('afterAnimate', null, self.elements);
+                    }
 
                 });
                 
